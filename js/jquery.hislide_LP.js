af119409a86a3ec8403296acb64b4dd2e0addd83
welly0007 <1020066@stemegypt.edu.eg>
2022-08-17 13:42:34 +0200

    first

---
+++ js/jquery.hislide_LP.js
@@ -0,0 +1,357 @@
+
+let queryTwo = window.matchMedia("(max-width:650px)");
+// var slidess = document.querySelector('.hi-slide');
+if(queryTwo.matches){
+    (function($) {
+        var slide = function(ele,options) {
+            var $ele = $(ele);
+            var setting = {
+                speed: 500,
+                interval: 2000,
+                
+            };
+            $.extend(true, setting, options);
+            var states = [
+                // { $zIndex: 1, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 2, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 3, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 4, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 5, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 6, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 7, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 8, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 9, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 10, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 11, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 12, width: 0, height: 150, top: 69, left: 400, $opacity: 0 },
+                { $zIndex: 8, width: 0, height: 170, top: 59, left: 400, $opacity: 0 },
+                { $zIndex: 9, width: 0, height: 0, top: 125, left: -200, $opacity: 1 },
+                { $zIndex: 10, width: 211, height: 320, top: 0, left: 0, $opacity: 1 },
+                { $zIndex: 9, width: 0, height: 0, top: 125, left: 368, $opacity: 1 },
+                { $zIndex: 8, width: 0, height: 250, top: 0, left: 400, $opacity: 0.0 },
+                // { $zIndex: 12, width: 0, height: 150, top: 69, left: 400, $opacity: 0 },
+                // { $zIndex: 11, width: 0, height: 0, top: 79, left: 500, $opacity: 0 },
+                // { $zIndex: 10, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 9, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 8, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 7, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 6, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 5, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 4, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 3, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 2, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 1, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 0, width: 0, height: 0, top: 79, left: 700, $opacity: 0 }
+            ];
+    
+            var $lis = $ele.find('li');
+            var timer = null;
+            const counter = document.getElementById('counter');
+            const max = document.getElementById('max'); 
+            let a = 1;
+            // 事件
+            $ele.find('.hi-next').on('click', function() {
+                next();
+            });
+            $ele.find('.hi-prev').on('click', function() {
+                states.push(states.shift());
+                move();
+                minus();
+            });
+            $ele.on('mouseenter', function() {
+                clearInterval(timer);
+                timer = null;
+            }).on('mouseleave', function() {
+                autoPlay();
+            });
+    
+            move();
+
+
+            function move() {
+                $lis.each(function(index, element) {
+                    var state = states[index];
+                    $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity);
+                });
+            }
+            function next() {
+                states.unshift(states.pop());
+                move();
+                if(a < max.innerText){                    a++;
+                    counter.innerText = a;
+                }else{
+                    a = 1;
+                    counter.innerText = a;
+
+                }
+            }
+    
+            function autoPlay() {
+                timer = setInterval(next, setting.interval);
+            }
+            function add(){
+                if(a < max.innerText){                    a++;
+                    counter.innerText = a;
+                }else{
+                    a = 1;
+                    counter.innerText = a;
+
+                }
+
+            }
+            function minus(){
+                if(a < max.innerText, a > 1 ){                    a--;
+                    counter.innerText = a;
+                }else if (a <= 1){
+                    a--;
+                    a = max.innerText;
+                    counter.innerText = a;
+
+                }else{
+                    a = 1
+                    counter.innerText = a;
+                }
+            }
+        }
+        $.fn.hiSlide = function(options) {
+            $(this).each(function(index, ele) {
+                slide(ele,options);
+            });
+            return this;
+        }
+    })(jQuery);
+}else{
+    let query = window.matchMedia("(max-width:1110px)");
+    if(query.matches){
+        (function($) {
+            var slide = function(ele,options) {
+                var $ele = $(ele);
+                var setting = {
+                    speed: 500,
+                    interval: 2000,
+                    
+                };
+                $.extend(true, setting, options);
+                var states = [
+                // { $zIndex: 1, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 2, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 3, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 4, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 5, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 6, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 7, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 8, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 9, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 10, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 11, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                // { $zIndex: 12, width: 0, height: 150, top: 69, left: 400, $opacity: 0 },
+                { $zIndex: 8, width: 0, height: 0, top: 59, left: 400, $opacity: 0 },
+                { $zIndex: 9, width: 0, height: 0, top: 125, left: -200, $opacity: 1 },
+                { $zIndex: 10, width: 211, height: 320, top: 0, left: 0, $opacity: 1 },
+                { $zIndex: 9, width: 211, height: 320, top: 0, left: 261, $opacity: 1 },
+                { $zIndex: 8, width: 0, height: 0, top: 125, left: 672, $opacity: 0.0 },
+                // { $zIndex: 12, width: 0, height: 150, top: 69, left: 400, $opacity: 0 },
+                // { $zIndex: 11, width: 0, height: 0, top: 79, left: 500, $opacity: 0 },
+                // { $zIndex: 10, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 9, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 8, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 7, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 6, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 5, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 4, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 3, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 2, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 1, width: 0, height: 0, top: 79, left: 700, $opacity: 0 },
+                // { $zIndex: 0, width: 0, height: 0, top: 79, left: 700, $opacity: 0 }
+                ];
+        
+                var $lis = $ele.find('li');
+                var timer = null;
+                const counter = document.getElementById('counter');
+                const max = document.getElementById('max'); 
+                let a = 1;
+                // 事件
+                $ele.find('.hi-next').on('click', function() {
+                    next();
+                });
+                $ele.find('.hi-prev').on('click', function() {
+                    states.push(states.shift());
+                    move();
+                    minus();
+                });
+                $ele.on('mouseenter', function() {
+                    clearInterval(timer);
+                    timer = null;
+                }).on('mouseleave', function() {
+                    autoPlay();
+                });
+        
+                move();
+
+
+                function move() {
+                    $lis.each(function(index, element) {
+                        var state = states[index];
+                        $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity);
+                    });
+                }
+                function next() {
+                    states.unshift(states.pop());
+                    move();
+                    if(a < max.innerText){                    a++;
+                        counter.innerText = a;
+                    }else{
+                        a = 1;
+                        counter.innerText = a;
+
+                    }
+                }
+        
+                function autoPlay() {
+                    timer = setInterval(next, setting.interval);
+                }
+                function add(){
+                    if(a < max.innerText){                    a++;
+                        counter.innerText = a;
+                    }else{
+                        a = 1;
+                        counter.innerText = a;
+
+                    }
+
+                }
+                function minus(){
+                    if(a < max.innerText, a > 1 ){                    a--;
+                        counter.innerText = a;
+                    }else if (a <= 1){
+                        a--;
+                        a = max.innerText;
+                        counter.innerText = a;
+
+                    }else{
+                        a = 1
+                        counter.innerText = a;
+                    }
+                }
+            }
+            $.fn.hiSlide = function(options) {
+                $(this).each(function(index, ele) {
+                    slide(ele,options);
+                });
+                return this;
+            }
+        })(jQuery);
+        
+    }else{
+        (function($) {
+            var slide = function(ele,options) {
+                var $ele = $(ele);
+                var setting = {
+                    speed: 500,
+                    interval: 2000,
+                    
+                };
+                $.extend(true, setting, options);
+                var states = [
+                    // { $zIndex: 1, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 2, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 3, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 4, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 5, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 6, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 7, width: 0, height: 150, top: 69, left: 400, $opacity: 0 },
+                    { $zIndex: 8, width: 0, height: 0, top: 198, left: -200, $opacity: 0.8 },
+                    { $zIndex: 9, width: 200, height: 302, top: 50, left: 0, $opacity: 0.8 },
+                    { $zIndex: 10, width: 281, height: 399, top: 0, left: 245, $opacity: 1 },
+                    { $zIndex: 9, width: 200, height: 302, top: 50, left: 558, $opacity: 0.8 },
+                    { $zIndex: 8, width: 0, height: 0, top: 198, left: 800, $opacity: 0.8 },
+                    // { $zIndex: 7, width: 0, height: 150, top: 69, left: 400, $opacity: 0 },
+                    // { $zIndex: 6, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 5, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 4, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 3, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 2, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 1, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                    // { $zIndex: 0, width: 0, height: 0, top: 79, left: 400, $opacity: 0 },
+                ];
+        
+                var $lis = $ele.find('li');
+                var timer = null;
+                const counter = document.getElementById('counter');
+                const max = document.getElementById('max'); 
+                let a = 1;
+                // 事件
+                $ele.find('.hi-next').on('click', function() {
+                    next();
+                });
+                $ele.find('.hi-prev').on('click', function() {
+                    states.push(states.shift());
+                    move();
+                    minus();
+                });
+                $ele.on('mouseenter', function() {
+                    clearInterval(timer);
+                    timer = null;
+                }).on('mouseleave', function() {
+                    autoPlay();
+                });
+        
+                move();
+
+
+                function move() {
+                    $lis.each(function(index, element) {
+                        var state = states[index];
+                        $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity);
+                    });
+                }
+                function next() {
+                    states.unshift(states.pop());
+                    move();
+                    if(a < max.innerText){                    a++;
+                        counter.innerText = a;
+                    }else{
+                        a = 1;
+                        counter.innerText = a;
+
+                    }
+                }
+        
+                function autoPlay() {
+                    timer = setInterval(next, setting.interval);
+                }
+                function add(){
+                    if(a < max.innerText){                    a++;
+                        counter.innerText = a;
+                    }else{
+                        a = 1;
+                        counter.innerText = a;
+
+                    }
+
+                }
+                function minus(){
+                    if(a < max.innerText, a > 1 ){                    a--;
+                        counter.innerText = a;
+                    }else if (a <= 1){
+                        a--;
+                        a = max.innerText;
+                        counter.innerText = a;
+
+                    }else{
+                        a = 1
+                        counter.innerText = a;
+                    }
+                }
+            }
+            $.fn.hiSlide = function(options) {
+                $(this).each(function(index, ele) {
+                    slide(ele,options);
+                });
+                return this;
+            }
+        })(jQuery);
+        
+    }
+}
+
--- js/main.js
+++ js/main.js
@@ -1,2 +1,204 @@
 (function($){"use strict"
-$(window).on('load',function(){$("#preloader").delay(600).fadeOut();});$('body').scrollspy({target:'#nav',offset:$(window).height()/2});$("#nav .main-nav a[href^='#']").on('click',function(e){e.preventDefault();var hash=this.hash;$('html, body').animate({scrollTop:$(this.hash).offset().top},600);});$('#back-to-top').on('click',function(){$('body,html').animate({scrollTop:0},600);});$('#nav .nav-collapse').on('click',function(){$('#nav').toggleClass('open');});$('.has-dropdown a').on('click',function(){$(this).parent().toggleClass('open-drop');});$(window).on('scroll',function(){var wScroll=$(this).scrollTop();wScroll>1?$('#nav').addClass('fixed-nav'):$('#nav').removeClass('fixed-nav');wScroll>700?$('#back-to-top').fadeIn():$('#back-to-top').fadeOut();});$('.work').magnificPopup({delegate:'.lightbox',type:'image'});$('#about-slider').owlCarousel({items:1,loop:true,margin:15,nav:true,navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],dots:true,autoplay:true,animateOut:'fadeOut'});$('#testimonial-slider').owlCarousel({loop:true,margin:15,dots:true,nav:false,autoplay:true,responsive:{0:{items:1},992:{items:2}}});})(jQuery);
+$(window).on('load',function(){$("#preloader").fadeOut();});$('body').scrollspy({target:'#nav',offset:$(window).height()/2});$("#nav .main-nav a[href^='#']").on('click',function(e){e.preventDefault();var hash=this.hash;$('html, body').animate({scrollTop:$(this.hash).offset().top},600);});$('#back-to-top').on('click',function(){$('body,html').animate({scrollTop:0},600);});$('#nav .nav-collapse').on('click',function(){$('#nav').toggleClass('open');});$('.has-dropdown a').on('click',function(){$(this).parent().toggleClass('open-drop');});$(window).on('scroll',function(){var wScroll=$(this).scrollTop();wScroll>1?$('#back-to-top').fadeIn():$('#back-to-top').fadeOut();});$('.work').magnificPopup({delegate:'.lightbox',type:'image'});$('#about-slider').owlCarousel({items:1,loop:true,margin:15,nav:true,navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],dots:true,autoplay:true,animateOut:'fadeOut'});$('#h4hovimonial-slider').owlCarousel({loop:true,margin:15,dots:true,nav:false,autoplay:true,responsive:{0:{items:1},992:{items:2}}});})(jQuery);
+
+    // set up text to print, each item in array is new line
+    var aText = new Array(
+        "STEM Model United",
+        "Nations",
+        ""
+        );
+        var iSpeed = 150; // time delay of print out
+        var iIndex = 0; // start printing array at this posision
+        var iArrLength = aText[0].length; // the length of the text array
+        var iScrollAt = 20; // start scrolling up at this many lines
+         
+        var iTextPos = 0; // initialise text position
+        var sContents = ''; // initialise contents variable
+        var iRow; // initialise current row
+
+        function typewriter()
+        {
+         sContents =  ' ';
+         iRow = Math.max(0, iIndex-iScrollAt);
+         var destination = document.getElementById("typedtext");
+         
+         while ( iRow < iIndex ) {
+          sContents += aText[iRow++] + '<br>';
+         }
+         destination.innerHTML = sContents  + aText[iIndex].substring(0, iTextPos) + "_";
+         if ( iTextPos++ == iArrLength ) {
+          iTextPos = 0;
+          iIndex++;
+          if ( iIndex != aText.length ) {
+           iArrLength = aText[iIndex].length;
+           setTimeout("typewriter()",200);
+          }
+         } else {
+          setTimeout("typewriter()", iSpeed);
+         }
+        };
+        
+
+        
+
+        window.addEventListener("load", function(){
+            typewriter();
+        });
+        //end type writer
+        // 
+        //DARK MODE
+        //
+        var choice = true;
+        var darkButt = document.querySelector('.dark')
+        var h2Clr = document.querySelectorAll('h2')
+        var h3Clr = document.querySelectorAll('h3')
+        var h4Clr = document.querySelectorAll('h4')
+        var h6Clr = document.querySelectorAll('h6')
+        var about = document.querySelectorAll('.about');
+        var h4hov = document.querySelectorAll('.h4hov');
+        var abouthov = document.querySelectorAll('.abouthov');
+        var team = document.querySelectorAll('.team')
+        var arrowTeam = document.querySelectorAll('.arrow')
+        var counterTeam = document.querySelector('.counter')
+        var footer = document.querySelector('footer')
+const toggle = document.getElementById('toggleDark');
+
+        for (let i = 0; i < abouthov.length; i++){
+            abouthov[i].addEventListener("mouseenter", function(){
+                    h4hov[i].style.color = "#fff";
+            });
+            abouthov[i].addEventListener("mouseleave", function(){
+                h4hov[i].style.color = "#10161a";
+                
+            });
+        }
+
+        toggle.addEventListener("click" , function(){
+            this.classList.toggle('bi-moon');
+            if(choice == true)
+            {
+                document.querySelector('body').style.background = '#1c1d21';
+                document.querySelector('body').style.color = '#868f9b';
+                document.querySelector('header').style.background = '#000';
+                document.querySelector('#become_delegate_butt').style.background = '#1c1d21'; 
+                counterTeam.style.color = '#fff'
+                for (let i = 0; i < h2Clr.length; i++){
+                    h2Clr[i].style.color = '#fff';
+
+                }
+                for (let i = 0; i < h3Clr.length; i++){
+                    h3Clr[i].style.color = '#fff';
+
+                }
+                for (let i = 0; i < h4Clr.length; i++){
+                    h4Clr[i].style.color = '#fff';
+
+                }
+                for (let i = 0; i < h6Clr.length; i++){
+                    h6Clr[i].style.color = '#fff';
+
+                }
+                for (let i = 0; i < about.length; i++){
+                    about[i].classList.add('about2');
+                    about[i].classList.remove('about3');
+                    }
+                    for (let i = 0; i < abouthov.length; i++){
+                        abouthov[i].addEventListener("mouseenter", function(){
+                                h4hov[i].style.color = "#10161a";
+                        });
+                        abouthov[i].addEventListener("mouseleave", function(){
+                            h4hov[i].style.color = "#fff";
+                            
+                        });
+                    }
+                for (let i = 0; i < team.length; i++){
+                    team[i].style.background = '#000';
+                    team[i].style.color = '#fff';
+                }
+                for (let i = 0; i < arrowTeam.length; i++){
+                    arrowTeam[i].style.color = '#fff';
+
+                }
+                footer.style.background = '#2d2d2d'
+                footer.style.color = '#fff'
+
+                document.querySelector('body').style.transition = '.8s ease-in-out';
+                document.querySelector('header').style.transition = '.8s ease-in-out';
+                document.querySelector('#become_delegate_butt').style.transition = '.8s ease-in-out';
+                toggle.style.color = '#fff';
+                
+                choice = false;
+            }
+            else
+            {
+                choice = true;
+                document.querySelector('body').style.background = '#fff'; 
+                document.querySelector('body').style.color = '#555a61'; 
+                document.querySelector('header').style.background = '#dddddd';
+                document.querySelector('#become_delegate_butt').style.background = '#6195ff'; 
+                counterTeam.style.color = '#10161a'
+
+                for (let i = 0; i < h2Clr.length; i++){
+                    h2Clr[i].style.color = '#10161a';
+
+                }
+                for (let i = 0; i < h3Clr.length; i++){
+                    h3Clr[i].style.color = '#10161a';
+
+                }
+                for (let i = 0; i < h4Clr.length; i++){
+                    h4Clr[i].style.color = '#10161a';
+
+                }
+                for (let i = 0; i < h6Clr.length; i++){
+                    h6Clr[i].style.color = '#10161a';
+                }
+                for (let i = 0; i < about.length; i++){
+                    about[i].classList.remove('about2');
+                    about[i].classList.add('about3');
+                }
+                for (let i = 0; i < abouthov.length; i++){
+                    abouthov[i].addEventListener("mouseenter", function(){
+                            h4hov[i].style.color = "#fff";
+                    });
+                    abouthov[i].addEventListener("mouseleave", function(){
+                        h4hov[i].style.color = "#10161a";
+                        
+                    });
+                }
+                for (let i = 0; i < team.length; i++){
+                    team[i].style.background = '#f4f4f4';
+                    team[i].style.color = '#10161a';
+                }
+                for (let i = 0; i < arrowTeam.length; i++){
+                    arrowTeam[i].style.color = '#10161a';
+
+                }
+                footer.style.background = '#1c1d21'
+                footer.style.color = '#555a61'
+                document.querySelector('body').style.transition = '.8s ease-in-out';
+                toggle.style.color = '#000';
+            }            
+        });
+        //
+        //END DARK MODE
+        //
+        // 
+        // Header
+        // 
+        header = document.querySelector('header');
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop){
+        header.style.top = "-80px";
+    } else {
+        header.style.top = "0px";
+    }
+    lastScrollTop = scrollTop;
+});
+        // 
+        // END Header
+        // 
+        
